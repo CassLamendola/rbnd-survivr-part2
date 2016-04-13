1c9ecb941cfc242616b53dda09247539e056a193
Cassandra Lamendola <cass.lamendola@gmail.com>
2016-04-12 20:44:46 -0700

    Add eliminate method to Game class, pass phase one

--- lib/game.rb
+++ lib/game.rb
@@ -24,6 +24,8 @@ class Game
 		@new_tribe = Tribe.new(tribe: merged_tribes, members: [])
 		@new_tribe.members << @tribe_one.members.first
 		@new_tribe.members << @tribe_two.members.first
+		clear_tribes
+		add_tribe(@new_tribe)
 		return @new_tribe
 	end
 
@@ -31,4 +33,15 @@ class Game
 		tribe = @tribes.sample
 		return tribe.members.sample
 	end
+
+	def eliminate
+		if immunity_challenge == @tribe_one
+			losing_member = @tribe_one.members.sample
+			return @tribe_one.members.delete(losing_member)
+		else
+			losing_member = @tribe_two.members.sample
+			return @tribe_two.members.delete(losing_member)
+		end
+	end
+
 end
